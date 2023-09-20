f1eff994ef96fbed01c3a37fe150d7c81cec2b27
Alfred Rubin <alfredo.rubin@neo4j.com>
2023-09-20 14:54:37 +0200

    changing main __init__

--- rdflib_neo4j/__init__.py
+++ rdflib_neo4j/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ["N10sNeo4jStore"]
+from config import Neo4jStoreConfig
+from config.const import HANDLE_MULTIVAL_STRATEGY,HANDLE_VOCAB_URI_STRATEGY
+__all__ = ["Neo4jStore",
+           "Neo4jStoreConfig",
+           "HANDLE_VOCAB_URI_STRATEGY",
+           "HANDLE_MULTIVAL_STRATEGY"]
